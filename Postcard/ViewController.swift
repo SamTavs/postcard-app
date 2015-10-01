dd5a9dcbe25f24f590c2affb1c3a1933b854722c
Sam Tavs <sam.taveras@gmail.com>
2015-10-01 09:30:50 -0700

    nameLabel

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var messageTextField: UITextField!
+    
     @IBOutlet weak var sendButton: UIButton!
     
     override func viewDidLoad() {
@@ -23,17 +26,24 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        // Keep the IBActions func clear
     }
 
     
     @IBAction func sendButtonPressed(sender: UIButton) {
             messageLabel.hidden = false
             messageLabel.text = messageTextField.text
-            messageLabel.textColor = UIColor.blueColor()
+            messageLabel.textColor = UIColor.blackColor()
+        
+            nameLabel.hidden = false
+            nameLabel.text = nameTextField.text
+            nameLabel.textColor = UIColor.blackColor()
         
             messageTextField.text = ""
             messageTextField.resignFirstResponder()
+        
+            nameTextField.text = ""
+            nameTextField.resignFirstResponder()
+        
         
             sendButton.setTitle("Sent" , forState: UIControlState.Normal)
         
